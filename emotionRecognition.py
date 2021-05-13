3efc1a987a1bdb38b347b066772f0c67d2a792d8
justcasualguy <kubachw@gmail.com>
2021-05-14 01:56:04 +0200

    Finished mfcc exctraction implementation

--- emotionRecognition.py
+++ emotionRecognition.py
@@ -6,6 +6,8 @@ import scipy.io.wavfile
 import matplotlib.pyplot as plt
 import numpy as np
 import mfcc
+import plots
+import librosa
 def getWavPath(oafOrYaf,emotion,word):
     rootPath = "TESS Speech Data"
     fullpath=os.path.join(rootPath,str.upper(oafOrYaf)+"_"+emotion,str.upper(oafOrYaf)+"_"+word+"_"+emotion+".wav")
@@ -13,19 +15,27 @@ def getWavPath(oafOrYaf,emotion,word):
 # x = np.array([[1], [4]], np.int32)
 frame_length=0.025
 frame_hop=0.01
+num_mfccs=12
 sample_rate, signal = scipy.io.wavfile.read(getWavPath("oaf","angry","back"))
+frame_size= int(round(frame_length * sample_rate)) #in samples
 signal = mfcc.pre_emphasis(signal,0.95)
 frames = mfcc.framing(signal,sample_rate,frame_length,frame_hop)
 frames_windowed = mfcc.hamming_window(frames, int(frame_length * sample_rate))
-frames_magnitude = mfcc.stft(frames_windowed,512)
-# flattened = np.array(frames_magnitude).flatten()
+frames_magnitude = mfcc.dft(frames_windowed)
+max= np.max(frames_magnitude)
+linspace = np.linspace(0,sample_rate/2,len(frames_magnitude[0]))
+frames_filtered=mfcc.mel_filter_banks(40,sample_rate,frames_magnitude)
+coefficients = mfcc.dct(frames_filtered)[:,1:num_mfccs+1]
+plots.plot_spectrogram(coefficients,signal,sample_rate)
+mfcc = librosa.feature.mfcc(frames_filtered,sample_rate)[1:13]
+plots.plot_spectrogram(mfcc,signal,sample_rate)
+len(coefficients)
 
-# signal_linespace=np.linspace(0,signal.shape[0]/sample_rate,signal.shape[0])
-signal_linespace=np.linspace(0,np.max(frames_magnitude[0]),frames_magnitude[0])
-plt.plot(signal_linespace,signal)
-plt.title("Fraza \"Say the word back\", emocja: złość")
-plt.xlabel("Czas [s]")
-plt.ylabel("Amplituda")
-plt.show()
+
+
+
+# signal_linespace=np.linspace(0,sample_rate,frames_magnitude[0])
+# mfcc.mel_filter_banks(40,sample_rate/2)
+
 
 
--- mfcc.py
+++ mfcc.py
@@ -1,21 +1,23 @@
 import numpy as np
+import scipy.fftpack as fftpack
+import librosa
 
-def pre_emphasis(signal,filter_coefficient):
-   return np.append(signal[0], signal[1:] - filter_coefficient * signal[:-1]) # returns pre-emphasized signal
+
+def pre_emphasis(signal, filter_coefficient):
+    return np.append(signal[0], signal[1:] - filter_coefficient * signal[:-1])  # returns pre-emphasized signal
+
 
 #
-#frame length - in seconds
-#frame hop - in seconds
+# frame length - in seconds
+# frame hop - in seconds
 #
 def framing(signal, sampling_rate, frame_length, frame_hop):
-    frame_size= frame_length * sampling_rate   # Convert from seconds to samples
-    frame_hop= frame_hop * sampling_rate
-    print(f"hop: {frame_hop}")
-    print(f"size: {frame_size}")
+    frame_size = frame_length * sampling_rate  # Convert from seconds to samples
+    frame_hop = frame_hop * sampling_rate
     signal_length = len(signal)
     frame_length = int(round(frame_size))
-    frame_hop= int(round(frame_hop))
-    num_frames = int((signal_length - frame_size) / frame_hop)+1 #calculate number of frames
+    frame_hop = int(round(frame_hop))
+    num_frames = int((signal_length - frame_size) / frame_hop) + 1  # calculate number of frames
 
     pad_signal_length = int(num_frames * frame_hop + frame_size)
     pad_zeros = np.zeros(pad_signal_length - signal_length)
@@ -26,17 +28,67 @@ def framing(signal, sampling_rate, frame_length, frame_hop):
     frames = pad_signal[indices.astype(np.int32, copy=False)]
     return frames
 
+
 #
 # frame length in samples
 #
-def hamming_window(frames,frame_length):
+def hamming_window(frames, frame_length):
     frames *= np.hamming(frame_length)
     return frames
 
-def stft(frames,stft_points):
-    frames_magnitude = np.absolute(np.fft.rfft(frames, stft_points))  # magnitude of the signal frames
+
+def dft(frames):
+    frames_magnitude = np.absolute(np.fft.rfft(frames))  # magnitude of the signal frames
     return frames_magnitude
 
-def power_spectrum(frames,stft_points):
+
+def power_spectrum(frames, stft_points):
     pow_spectrum_frames = ((1.0 / stft_points) * ((frames) ** 2))  # power spectrum of signal frames
     return pow_spectrum_frames
+
+
+# Returs frames filetered using mel filter banks
+# filters_num - number of filter banks
+# sample_rate - signal sample rate
+# frame_size - size of a frame used in dft
+def mel_filter_banks(filters_count, sample_rate, frames):
+    mel_min = 0
+    mel_max = freqToMelFreq(sample_rate / 2)
+    mel_bands = np.linspace(mel_min, mel_max, filters_count + 2)
+    freq_bands = np.array(list(map(melFreqToFreq, mel_bands)))
+    frame_size = len(frames[0])
+
+    # frequency of each magnitude
+    mag_freqs = np.linspace(0, sample_rate / 2, frame_size)
+    filtered_frames = np.zeros((len(frames), frame_size))
+    # apply filterbank to each magnitude in each frame
+    for f, frame in enumerate(frames):
+        for m, mag_freq in enumerate(mag_freqs):
+
+            for fb in range(1, len(freq_bands) - 1):
+                left = freq_bands[fb - 1]
+                center = freq_bands[fb]
+                right = freq_bands[fb + 1]
+                if mag_freq >= left and mag_freq <= center:
+                    weight = 2 * (mag_freq - left) / (center - left)
+                    filtered_frames[f, m] += frame[m] ** 2 * weight
+                elif mag_freq <= right and mag_freq > center:
+                    weight = 2 * (right - mag_freq) / (right - center)
+                    filtered_frames[f, m] += frame[m] ** 2 * weight
+
+    filtered_frames = np.where(filtered_frames == 0, np.finfo(float).eps, filtered_frames)
+    filtered_frames = 20 * np.log10(filtered_frames)
+
+    return filtered_frames
+
+
+def freqToMelFreq(freq):
+    return 2595 * np.log10(1 + freq / 700)
+
+
+def melFreqToFreq(mel_freq):
+    return 700 * (np.power(10, mel_freq / 2595) - 1)
+
+
+def dct(frames):
+    return fftpack.dct(frames)
